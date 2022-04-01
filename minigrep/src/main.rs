e4b61a30b71cb321d27bb0c705e0ccd0c44041bd
chairbender <kwhipke1@gmail.com>
2022-04-01 15:26:26 -0700

    v2 minigrep

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing args: {}", err);
+        eprintln!("Problem parsing args: {}", err);
         process::exit(1);
     });
 
@@ -13,7 +13,7 @@ fn main() {
     println!("In file {}", config.filename);
 
     if let Err(e) = minigrep::run(config) {
-        println!("App error: {}", e);
+        eprintln!("App error: {}", e);
 
         process::exit(1);
     }
